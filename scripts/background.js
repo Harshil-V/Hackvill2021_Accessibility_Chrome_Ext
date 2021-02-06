eba3ab9eeb103e6001107559e7a7272ee919d880
Joseph <56702275+joeguy57@users.noreply.github.com>
2021-02-06 16:20:42 -0700

    Completed Text-To-Speech

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,14 @@
-document.addEventListener("onload",()=>{
+    function textToSpeech(text) {
+        let info = new SpeechSynthesisUtterance();
+        info.volume = 1;
+        info.rate = 1;
+        info.pitch = 3;
+        info.lang = 'en-us';
+        info.text = text;
+        speechSynthesis.speak(info);
+    }
 
-})
+    document.onmouseup = function() {
+        let text = chrome.tabs.getSelection();
+        textToSpeech(text.toString());
+    };
